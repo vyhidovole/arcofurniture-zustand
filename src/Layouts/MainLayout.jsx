3c3f98b263cd3c50b578e8b788bf9bcbecfa5a2c
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-04-28 12:33:12 +0300

    children inserted

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navigation from "@/components/Navigation";
-
+import Footer from "@/components/Footer";
 /* Обертка контента основной страницы */
-const MainLayout = () => {
+const MainLayout = ({children}) => {
   return (
     <main>
       <Navigation />
-      <div className="container p-4 bg-slate-400">This is main page</div>
+      <div className="container p-4 bg-slate-400">{children}</div>
+      <Footer/>
     </main>
   );
 };
