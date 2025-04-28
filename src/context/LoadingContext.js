a1d22131c3e1107c467bc66833abbe54b689b816
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-04-28 12:32:03 +0300

    No change

--- src/context/LoadingContext.js
+++ src/context/LoadingContext.js
@@ -20,7 +20,7 @@ export const LoadingProvider = ({children})=>{
     const[loading,setLoading]= useState(false)
 
     return (
-        <LoadingContext.Provider value={{loading,setLoading}}>
+        <LoadingContext.Provider value={{ loading, setLoading} }>
             {children}
         </LoadingContext.Provider>
     )
