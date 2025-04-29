3f1fc0ceb97c8afe28cdf02d10d14a6ca3afeb02
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-04-29 10:10:05 +0300

    providers added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,16 +2,21 @@ import "@/styles/globals.css";
 import React from "react";
 import MainLayout from "../Layouts/MainLayout";
 import { LoadingProvider } from '@/context/LoadingContext';
-
+import { ThemeProvider } from '@/context/ThemeContext'; 
+import { CartProvider } from '@/context/CartContext';
 import "@/styles/globals.css";
 
 /* Корневой элемент страницы */
 const App = ({ Component, pageProps }) => {
   return (
     <LoadingProvider>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <ThemeProvider>
+      <CartProvider>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+        </CartProvider>
+      </ThemeProvider>
     </LoadingProvider>
 
   );
