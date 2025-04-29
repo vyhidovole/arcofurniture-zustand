02eb569d8af82b5f43f0a3c72dafee62b16778bc
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-04-29 10:09:11 +0300

    import added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 /* Обертка контента основной страницы */
 const MainLayout = ({children}) => {
   return (
     <main>
       <Navigation />
+      <Header/>
       <div className="container p-4 bg-slate-400">{children}</div>
       <Footer/>
     </main>
