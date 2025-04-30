206e6f8b26abe8a67c82666a3d36193f5f0ed916
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-04-30 08:31:05 +0300

    Navigation fixed

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import Skeleton from "react-loading-skeleton";//Ипортируем секлетон
 import { useLoading } from "@/context/LoadingContext";//Импортируем хук useSkeleton
 import 'react-loading-skeleton/dist/skeleton.css';//Импортируем стили скелетона
-import { useTheme } from "next-themes";
+import { useTheme } from '@/context/ThemeContext';
 import '@/components/Header.css';
 
 /*пункты меню в шапке*/
@@ -18,114 +18,115 @@ const navItems = [
 ];
 /**
  * Компонент Navigation отображает навигационное меню с пунктами,
- * а так же переключатель темы. Он использует контексты для управления 
- * состоянием загрузки темы.
- * 
+ * а также переключатель темы. Он использует контексты для управления
+ * состоянием загрузки и темы.
+ *
  * @component
  * @returns {JSX.Element} Элемент, представляющий навигационное меню.
- * 
+ *
  * @example
- * <Navigtion/>
- * 
+ * <Navigation />
  */
+
 const Navigation = () => {
 
-    const { isDarkMode, toggleTheme } = useTheme();// Получаем доступ к теме
-    const { loading, setLoading } = useLoading()//Получаем состояние загрузки
-    //состояние для активного пункта меню
+    const { isDarkMode, toggleTheme } = useTheme(); // Получаем доступ к теме
+    const { loading, setLoading } = useLoading(); // Получаем состояние загрузки
+    // состояние (стейт) для активного пункта меню
     const [activeLink, setActiveLink] = useState("Главная");
-
+  
     const router = useRouter();
     useEffect(() => {
-        const currentPath = router.pathname;
-        const activeItem = navItems.find(item => item.path === currentPath);
-        if (activeItem) {
-            setActiveLink(activeItem.name);
-        }
+      const currentPath = router.pathname;
+      const activeItem = navItems.find(item => item.path === currentPath);
+      if (activeItem) {
+        setActiveLink(activeItem.name);
+      }
     }, [router.pathname]);
-
-    //клик по активному пункту меню
+  
+    // клик по активному пункту меню
     const onClickHandler = (link, path) => {
-        if (link !== activeLink) {
-            setLoading(true);//Устанавливаем состояние загрузки
-            router.push(path).then(() => {
-                setLoading(false);// Откючаем состояние загрузки после перехода
-            });
-            setActiveLink(link)
-        }
+      if (link !== activeLink) {
+        setLoading(true); // Устанавливаем состояние загрузки
+        router.push(path).then(() => {
+          setLoading(false); // Отключаем состояние загрузки после перехода
+        });
+        setActiveLink(link);
+      }
+      console.log(activeLink)
     };
-
+  
     return (
-        <header className={`shadow ${isDarkMode ? 'bg-gray-800' : 'bg-white'} h-16 flex items-center`} >
-            <div className="container flex justify-between">
-                {/*Левая часть с первыми пятью пунктами меню */}
-                <nav className="flex item-center gap-5">
-                    {loading ? (
-                        //Отображаем скелетон, пока данные загружаются
-                        Array(5).fill().map((_, index) => (
-                            <Skeleton key={index} width={100} height={20} />
-                        ))
-                    ) : (
-                        navItems.slice(0, 5).map((item) => (
-                            <a
-                                onClick={() => onClickHandler(item.name, item.path)}//Передаём путь
-                                className={`nav-link cursor-pointer
-                        ${item.name === activeLink ?
-                                        "text-sky-500 underline-animation" : "text-gray-800"
-                                    }${isDarkMode ? 'dark:text-sky-500' : 'text-gray-800'}`}
-                                key={item.path}
-                            >
-                                {item.name}
-                            </a>
-                        ))
-                    )}
-                </nav>
-                {/*Правая часть с полследними двумя пунктами меню */}
-
-                <nav className="flex items-center gap-5">
-                    {loading ? (
-                        //Отображаем скелетон для правой части меню
-                        Array(2).fill().map((_, index) => (
-                            <Skeleton key={index} width={80} height={20} />
-                        ))
-                    ) : (
-                        navItems.slice(5).map((item) => (
-                            <a
-                                onClick={() => onClickHandler(item.name, item.path)}
-                                className={`nav-link cursor-pointer
-                            ${item.name === activeLink ? "text-sky-500 underline-animation" : "text-gray-800"
-                                    }`}
-                                key={item.path}
-                            >
-                                {item.name}
-                            </a>
-                        ))
-                    )}
-                    {/*Перключатель темы */}
-                    <fieldset className="relative inline-block w-16 h-8">
-                        <input type="checkbox"
-                            className="switch opacity-0 w-0 h-0"
-                            id="theme-toggle"
-                            checked={isDarkMode}
-                            onChange={toggleTheme}
-                        />
-                        <label
-                            htmlFor="them-toggle"
-                            className={`absolute cursor-pointer top-0 right-0 bottom-0 
-                                rounded-full transition duration-300
-                        ${isDarkMode ? 'bg-blue-600' : 'bg-gray-300'}`}
-                        >
-                        </label>
-                        <span onClick={toggleTheme}
-                            className={`absolute left-1 top-1 block w-6 h-6
-                            bg-white rounded-full transition-transform duration-300
-                            ${isDarkMode ? 'translate-x-8' : ''}`}
-                        >
-                        </span>
-                    </fieldset>
-                </nav>
-            </div>
-        </header>
-    )
-}
-export default Navigation
+      <header className={`shadow ${isDarkMode ? 'bg-gray-800' : 'bg-white'} h-16 flex items-center`}>
+        <div className="container flex justify-between">
+          {/* Левая часть с первыми пятью пунктами меню */}
+          <nav className="flex items-center gap-5">
+            {loading ? (
+              // Отображаем скелетон, пока данные загружаются
+              Array(5).fill().map((_, index) => (
+                <Skeleton key={index} width={100} height={20} />
+              ))
+            ) : (
+              navItems.slice(0, 5).map((item) => (
+                <a
+                  onClick={() => onClickHandler(item.name, item.path)}//передаем путь
+                  className={`nav-link cursor-pointer
+                   ${item.name === activeLink ?
+                      "text-sky-500  underline-animation " : "text-gray-800"
+                    }${isDarkMode ? 'dark:text-sky-500' : 'text-gray-800'} `}
+                  key={item.path}
+                >
+                  {item.name}
+                </a>
+              ))
+            )}
+          </nav>
+  
+          {/* Правая часть с последними двумя пунктами меню */}
+  
+          <nav className="flex items-center gap-5">
+            {loading ? (
+              // Отображаем скелетон для правой части меню
+              Array(2).fill().map((_, index) => (
+                <Skeleton key={index} width={80} height={20} />
+              ))
+            ) : (
+              navItems.slice(5).map((item) => (
+                <a
+                  onClick={() => onClickHandler(item.name, item.path)}
+                  className={`nav-link cursor-pointer 
+                  ${item.name === activeLink ? "text-sky-500 underline-animation " : "text-gray-800"
+                    }`}
+                  key={item.path}
+                >
+                  {item.name}
+                </a>
+              ))
+            )}
+            {/* Переключатель темы */}
+            <fieldset className="relative inline-block w-16 h-8">
+              <input
+                type="checkbox"
+                className="switch opacity-0 w-0 h-0"
+                id="theme-toggle"
+                checked={isDarkMode}
+                onChange={toggleTheme}
+              />
+              <label
+                htmlFor="theme-toggle"
+                className={`absolute cursor-pointer top-0 left-0 right-0 bottom-0 rounded-full transition duration-300
+                   ${isDarkMode ? 'bg-blue-600' : 'bg-gray-300'}`}
+              ></label>
+              <span onClick={toggleTheme} 
+              className={`absolute left-1 top-1 block w-6 h-6
+                 bg-white rounded-full transition-transform duration-300
+                  ${isDarkMode ? 'translate-x-8' : ''}`}>
+              </span>
+            </fieldset>
+          </nav>
+        </div>
+      </header>
+    );
+  };
+  
+  export default Navigation;
