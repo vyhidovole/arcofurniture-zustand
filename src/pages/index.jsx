0c08f52f751d9f5e8f00a556dab99aa6a066258d
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-05-01 10:46:38 +0300

    Catalogue component added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,19 @@
 import Head from "next/head";
 import React from "react";
+import Catalogue from "@/components/Catalogue";
 
 /* Домашнаяя страница */
 const Home = () => (
+  <>
   <Head>
     <title>Arcofurniture</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
+  <Catalogue/>
+  </>
+  
 );
 
 export default Home;
