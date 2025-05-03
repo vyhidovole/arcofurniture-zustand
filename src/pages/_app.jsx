ceb94cbefa013285a80cc470c0adda6f85bc980b
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-05-03 13:29:55 +0300

    JS Doc added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,20 @@ import { ThemeProvider } from '@/context/ThemeContext';
 import { CartProvider } from '@/context/CartContext';
 import "@/styles/globals.css";
 
-/* Корневой элемент страницы */
+/**
+ * Корневой элемент страницы.
+ * Этот компонент оборачивает все страницы приложения в необходимые провайдеры
+ * и предоставляет общий макет для всех страниц.
+ *
+ * @component
+ * @param {Object} props - Свойства компонента.
+ * @param {React.ElementType} props.Component - Компонент страницы, который будет рендериться.
+ * @param {Object} props.pageProps - Свойства, передаваемые компоненту страницы.
+ * @returns {JSX.Element} Элемент, представляющий корневой компонент приложения.
+ *
+ * @example
+ * <App Component={MyPage} pageProps={myPageProps} />
+ */
 const App = ({ Component, pageProps }) => {
   return (
     <LoadingProvider>
