201b02a6e781ae368a912949726145a05a74c433
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-05-17 11:41:02 +0300

    upgrade styles

--- src/components/ui/Dropdown/Dropdown.jsx
+++ src/components/ui/Dropdown/Dropdown.jsx
@@ -22,7 +22,7 @@ const Dropdown = () => {
             </div>
 
             {isOpen && (
-                <div className="absolute right-0 z-10 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                <div className="absolute right-0 z-10 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black/20">
                     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                         <Link href="/kitchen"className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
                             Угловые кухни
